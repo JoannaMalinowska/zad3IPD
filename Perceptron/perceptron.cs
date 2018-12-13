e6b1a28b123df95b83d0203b65497db562b171d8
221627 Joanna Malinowska <221627@edu.p.lodz.pl>
2018-12-13 10:44:22 +0100

    initial commit

---
+++ Perceptron/perceptron.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    class perceptron
+    {
+        double[] Weight;
+        int iteration;
+        int setNumber;
+
+        class zestaw
+        {
+            int[] input { get; set; }
+            int[] output { get; set; }
+            public zestaw(int[] input, int[] output)
+            {
+                this.output = output;
+                this.input = input;
+            }
+        }
+        public void init()
+        {
+            //wejscia i wyjscia są w zadaniu identyczne
+
+            zestaw zestaw1 = new zestaw(new int[] { 1, 0, 0, 0 }, new int[] { 1, 0, 0, 0 });
+            zestaw zestaw2 = new zestaw(new int[] { 0, 1, 0, 0 }, new int[] { 0, 1, 0, 0 });
+            zestaw zestaw3 = new zestaw(new int[] { 0, 0, 1, 0 }, new int[] { 0, 0, 1, 0 });
+            zestaw zestaw4 = new zestaw(new int[] { 0, 0, 0, 1 }, new int[] { 0, 0, 0, 1 });
+
+
+
+
+
+
+        }
+    }
+}
